6dc60a86c5649107c92dc1a5cd241f14eaf5af7a
Andrés Bentos <asbentos@hotmail.com>
2019-04-21 10:52:27 -0300

    # Agrega opción de borrar desde la lista o grid. De forma simple y a nivel de UI. >># Esta bueno para reutilizar algo de acá, con cosas como los favoritos y eso.

--- app/src/main/java/com/example/listviews/GridActivity.java
+++ app/src/main/java/com/example/listviews/GridActivity.java
@@ -2,6 +2,7 @@ package com.example.listviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -40,14 +41,16 @@ public class GridActivity extends AppCompatActivity {
 
          myAdapter = new MyAdapter(this,R.layout.grid_item,names);
         gridView.setAdapter(myAdapter);
+        registerForContextMenu(gridView);
     }
+    //Inflamos el menu de opciones
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.action_bar_menu,menu);
         return true;
     }
-
+    //Manejamos evento clic en el MEnu de opciones.
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
@@ -60,9 +63,33 @@ public class GridActivity extends AppCompatActivity {
 
             default:
                 return super.onOptionsItemSelected(item);
-
         }
+    }
+    //Inflamos el layout del context menu
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        menu.setHeaderTitle("Se borró: "+ names.get(info.position));
+        inflater.inflate(R.menu.context_menu,menu);
+    }
+    //Manejamos eventos click del context menu.
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
 
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
 
+        switch (item.getItemId()){
+            case (R.id.deleted_item):
+                //Borramos item cliqueado , viene dada la posición del mismo
+                this.names.remove(info.position);
+                //Notificamos al adaptador
+                this.myAdapter.notifyDataSetChanged();
+
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
