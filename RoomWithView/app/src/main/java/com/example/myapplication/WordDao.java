f3c3b6e5f462fbc5cc4bc730eb72bd508376404d
Andrés Bentos <asbentos@hotmail.com>
2019-04-24 16:19:21 -0300

    Agrega un retonro de Livedata para el getAllWords)

--- RoomWithView/app/src/main/java/com/example/myapplication/WordDao.java
+++ RoomWithView/app/src/main/java/com/example/myapplication/WordDao.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
@@ -28,6 +29,6 @@ public interface WordDao {
     @Query("DELETE FROM word_table")
     void deleteAll();
 
-    @Query("SELECT * FROM word_table ORDER BY word ASC")
-    List<Word> getAllWords();
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAllWords();
 }
